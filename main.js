e50a1981089118901d00eff994c447194484650e
Ahmed Ramadan <Ahmed @gmil.com>
2026-09-03 23:01:59 +0300

    update file main.js

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mysql from 'mysql2';
-
+import dotenv from 'dotenv';
+dotenv.config({ path: './Config/dev.env' });
 
 const port = process.env.PORT || 4500;
 const app = express();
